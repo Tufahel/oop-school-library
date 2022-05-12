fb0e355cb21ca878389abe0589bf7941b9a316cd
Tufahel <tufahel96@gmail.com>
2022-05-13 02:08:56 +0600

    linters fixed with adding super and removing testcase

--- nameable.rb
+++ nameable.rb
@@ -1,30 +1,30 @@
 class Nameable
-    def correct_name
-        raise NotImplementError
-    end
+  def correct_name
+    raise NotImplementError
+  end
 end
 
 class BaseDecorator < Nameable
-    attr_accessor :nameable
+  attr_accessor :nameable
 
-    def initialize(nameable)
-        super()
-        @nameable = nameable
-    end
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
 
-    def correct_name
-        @nameable.correct_name
-    end
+  def correct_name
+    @nameable.correct_name
+  end
 end
 
 class CapitalizeDecorator < BaseDecorator
-    def correct_name
-        @nameable.correct_name.capitalize
-    end
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
 end
 
 class TrimmerDecorator < BaseDecorator
-    def correct_name
-        @nameable.correct_name[0..10]
-    end
+  def correct_name
+    @nameable.correct_name[0..10]
+  end
 end
--- person.rb
+++ person.rb
@@ -5,6 +5,7 @@ class Person < Nameable
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -25,10 +26,3 @@ class Person < Nameable
 
   private :of_age?
 end
-
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalizedPerson = CapitalizeDecorator.new(person)
-capitalizedPerson.correct_name
-capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-capitalizedTrimmedPerson.correct_name
