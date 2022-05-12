621000ef2283abc51c20825c59719b1bf376668d
Tufahel <tufahel96@gmail.com>
2022-05-13 01:55:30 +0600

    decorators implemented

--- nameable.rb
+++ nameable.rb
@@ -1,7 +1,30 @@
 class Nameable
-    attr_accessor :nameable
-
     def correct_name
         raise NotImplementError
+    end
+end
+
+class BaseDecorator < Nameable
+    attr_accessor :nameable
+
+    def initialize(nameable)
+        super()
+        @nameable = nameable
+    end
+
+    def correct_name
+        @nameable.correct_name
+    end
+end
+
+class CapitalizeDecorator < BaseDecorator
+    def correct_name
+        @nameable.correct_name.capitalize
+    end
+end
+
+class TrimmerDecorator < BaseDecorator
+    def correct_name
+        @nameable.correct_name[0..10]
     end
 end
--- person.rb
+++ person.rb
@@ -23,5 +23,12 @@ class Person < Nameable
     @name
   end
 
-  private :is_of_age?
+  private :of_age?
 end
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+capitalizedTrimmedPerson.correct_name
