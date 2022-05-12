29524d72a34542cd2f59736f8497739ada81c9db
Tufahel <tufahel96@gmail.com>
2022-05-12 15:10:55 +0600

    worked on review changes

--- student.rb
+++ student.rb
@@ -3,9 +3,9 @@ require_relative('./person')
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(classroom, age, name, parent_permission)
-    @classroom = classroom
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
+    @classroom = classroom
   end
 
   def play_hooky
