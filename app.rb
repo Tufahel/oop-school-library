9026557301c06d47e56846037ef4fa7ea1820a18
Tufahel <tufahel96@gmail.com>
2022-05-14 21:31:30 +0600

    console setup

--- app.rb
+++ app.rb
@@ -11,4 +11,17 @@ class App
         @persons = []
         @rentals = []
     end
+
+    def console_start
+        puts 'Welcome to my School Library!'
+        until list_of_options
+            input = gets.chomp
+            if input == '7'
+                puts 'Thank you for using my School Library!'
+                break
+            end
+
+            option input
+        end
+    end
 end
