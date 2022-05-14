792129e200e681ed26a305296e4e0f1ce787e783
Tufahel <tufahel96@gmail.com>
2022-05-14 23:30:46 +0600

    rentals listed

--- app.rb
+++ app.rb
@@ -101,4 +101,18 @@ class App
 
         puts 'Rental created successfully'
     end
+
+    def list_all_rentals
+        print 'To see person rentals enter the person ID: '
+        id = gets.chomp.to_i
+        puts 'Rented Books:'
+        @rentals.each do |rental|
+            if rental.person.id == id
+                puts "Person: #{rental.person.name} Date: #{rental.date}, Book '#{rental.book.title}' by #{rental.book.author}"
+            else
+                puts
+                puts 'No records where found for the given ID'
+            end
+        end
+    end
 end
