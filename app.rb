51c8fa662e98f582e0342bad4a09eb2a4a3df22f
Tufahel <tufahel96@gmail.com>
2022-05-14 21:35:49 +0600

    list all books

--- app.rb
+++ app.rb
@@ -26,6 +26,8 @@ class App
     end
 
     def list_all_books
+        puts 'Database is empty! Add a book.' if @books.empty?
+        @books.each { |book| puts "[Book] Title: #{book.title}, Author: #{book.author}"}
     end
 
     def list_all_persons
