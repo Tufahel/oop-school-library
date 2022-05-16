09036c52fa3cbf6e4f41ee52ed214ef260506daa
Tufahel <tufahel96@gmail.com>
2022-05-16 19:11:29 +0600

    console-start function added in new class startApp

--- app.rb
+++ app.rb
@@ -4,25 +4,13 @@ require_relative './student'
 require_relative './classroom'
 require_relative './book'
 require_relative './rental'
+require_relative './start_app.rb'
 
-class App
+class App < StartApp
   def initialize
     @books = []
     @persons = []
     @rentals = []
-  end
-
-  def console_start
-    puts 'Welcome to my School Library!'
-    until list_of_options
-      input = gets.chomp
-      if input == '7'
-        puts 'Thank you for using my School Library!'
-        break
-      end
-
-      option input
-    end
   end
 
   def list_all_books
---
+++ start_app.rb
@@ -0,0 +1,15 @@
+class StartApp
+    def console_start
+        puts 'Welcome to my School Library!'
+        until list_of_options
+          input = gets.chomp
+          if input == '7'
+            puts 'Thank you for using my School Library!'
+            break
+          end
+    
+          option input
+        end
+    end
+end
+      
